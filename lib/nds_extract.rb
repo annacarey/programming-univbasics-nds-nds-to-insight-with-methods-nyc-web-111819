e394ef77e6a4e8ed1fcfe664720c9a3957440b0e
declarative-blog-8005 <anna@annajcarey.com>
2019-11-01 20:11:14 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -13,7 +13,7 @@ end
 # Find a way to accumulate the :worldwide_grosses and return that Integer
 # using director_data as input
 def gross_for_director(director_data)
-  p director_data
+  pp director_data
   total = 0 
   index = 0 
   p director_data[:movies][index].length
